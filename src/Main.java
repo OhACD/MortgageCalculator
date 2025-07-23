d9d3cd75421dfb1290cbb03ff1c1f3c989cdc229
OhACD <imadeldeen007@gmail.com>
2025-07-23 10:53:59 +0300

    Easier usability

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,50 @@ public class Main {
     public static void main(String[] args) {
         final int PERCENT = 100;
         final int MONTHS_IN_YEAR = 12;
+        int principal;
+        int period;
+        double rate;
+        double mortgage;
+        String mortgageFormatted;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Principal:");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1k - $1M):");
+            principal = scanner.nextInt();
 
-        System.out.println("Annual intrest rate:");
-        double rate = (scanner.nextDouble() / PERCENT) / MONTHS_IN_YEAR;
+            if (principal < 1000 || principal > 1_000_000) {
+                System.out.println("You need to enter a value between $1k and $1M");
+                continue;
+            } break;
+        }
 
-        System.out.println("Period (Years):");
-        int period = scanner.nextInt() * MONTHS_IN_YEAR;
-            double mortgage = principal * // Dont Multiply by period you goose
+        while (true) {
+            System.out.print("Annual intrest rate:");
+            rate = scanner.nextDouble();
+
+            if (rate < 1 || rate >= 30) {
+                System.out.println("You need to enter a value greater than 0 and less than or equals to 30");
+                continue;
+            } break;
+        }
+            rate = (rate / PERCENT) / MONTHS_IN_YEAR;
+
+        while (true) {
+            System.out.print("Period (Years):");
+            period = scanner.nextInt();
+
+            if (period < 1 || period > 30){
+                System.out.println("You need to enter a value greater than 0 and less than 30");
+                continue;
+            } break;
+        }
+            period = period * MONTHS_IN_YEAR;
+
+            mortgage = principal * // Dont Multiply by period you goose
                     (rate * Math.pow(rate + 1, period)) /
                     (Math.pow(rate + 1, period) - 1);
             mortgage = Math.round(mortgage); // Rounding it to the nearst whole number
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+            mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
 
         System.out.println("Mortgage: " + mortgageFormatted);
     }
